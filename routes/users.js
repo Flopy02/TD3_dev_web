4c900bfc472f6145342a52ab39decd791128d4c8
flopy02 <flo.barege@gmail.com>
2024-10-24 14:50:34 +0200

    ajout tableau

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const router = express.Router()
 
+const usersArray =[
+    { id: 1, firstName: "John", lastName: "Doe", role: "admin" },
+	{ id: 2, firstName: "Jane", lastName: "Smith", role: "user" },
+	{ id: 3, firstName: "Alice", lastName: "Johnson", role: "moderator" },
+	{ id: 4, firstName: "Bob", lastName: "Brown", role: "user" },
+	{ id: 5, firstName: "Charlie", lastName: "Davis", role: "admin" },
+]
 router.get("/users", (req, res) => {
     res.json({
         msg: "Ceci est un test depuis le routeur",
