7ff3856d92c2d50ab5368000323f810474ff537f
flopy02 <flo.barege@gmail.com>
2024-10-24 14:39:12 +0200

    création d'une route utilisateur

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("test from node");
+const express = require("express")
+const app = express()
+const port = 3000
+const usersRouter = require("./routes/users.js")
+
+
+//MIDDLEWARE
+app.use(express.json())
+//users endpoint
+app.use("/api/", usersRouter)
+//users endpoint
+app.use("/api/",usersRouter)
+
+app.listen(port,()=>{
+    console.log('Server running at http://localhost:${port}')
+})
